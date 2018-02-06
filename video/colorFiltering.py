446914967ec084b644175a53946cf30525701bd0
SavorTheFlavor2 <2500364530@qq.com>
2018-02-06 23:57:07 +0800

    color filter (smooth and blur)

--- video/colorFiltering.py
+++ video/colorFiltering.py
@@ -17,8 +17,17 @@ while True:
 	mask = cv2.inRange(hsv, lower_red, upper_red)
 	res = cv2.bitwise_and(frame, frame, mask=mask) # frame&frame in the mask area
 	
+	# smoothed and blur
+	kernel = np.ones((15,15), np.float32)/255 # identity matrix /255
+	smoothed = cv2.filter2D(res, -1, kernel)
+	
+	blur = cv2.GaussianBlur(res, (15,15), 0)
+	median = cv2.medianBlur(res, 15)
+	bilateral = cv2.bilateralFilter(res, 15, 75, 75)
+	
 	cv2.imshow('frame',frame)
-	cv2.imshow('res',res)
+	# cv2.imshow('res',res)
+	cv2.imshow('median',median)
 	
 	# k = cv2.waitKey(5) & 0xFF
 	# if k == 27:
