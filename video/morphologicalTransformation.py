37201dddac361d1504c6688eee6dbd4cad25afd4
SavorTheFlavor2 <2500364530@qq.com>
2018-02-15 23:56:03 +0800

    morphology transformation

--- video/morphologicalTransformation.py
+++ video/morphologicalTransformation.py
@@ -17,17 +17,17 @@ while True:
 	mask = cv2.inRange(hsv, lower_red, upper_red)
 	res = cv2.bitwise_and(frame, frame, mask=mask) # frame&frame in the mask area
 	
-	# smoothed and blur
-	kernel = np.ones((15,15), np.float32)/255 # all elements are 1  (/255
-	smoothed = cv2.filter2D(res, -1, kernel)
+	kernel = np.ones((5,5), np.uint8)
+	erosion = cv2.erode(mask, kernel, iterations = 1)
+	dilation = cv2.dilate(mask, kernel, iterations = 1)
 	
-	blur = cv2.GaussianBlur(res, (15,15), 0)
-	median = cv2.medianBlur(res, 15)
-	bilateral = cv2.bilateralFilter(res, 15, 75, 75)
+	opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+	closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 	
 	cv2.imshow('frame',frame)
-	# cv2.imshow('res',res)
-	cv2.imshow('median',median)
+	cv2.imshow('dilation',dilation)
+	cv2.imshow('opening',opening)
+	cv2.imshow('closing',closing)
 	
 	# k = cv2.waitKey(5) & 0xFF
 	# if k == 27:
